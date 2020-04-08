5033c1ea3dc120118a77178c4e8b31571c9eefd9
Emily A. Williams <e.a.williams@leeds.ac.uk>
2020-04-08 17:01:26 +0100

    Update imdb_ratings_playaround.R

--- imdb_ratings_playaround.R
+++ imdb_ratings_playaround.R
@@ -1,12 +1,17 @@
 #load relevant package libraries
 library(tidyverse)
+library(viridis)  # for colour palette (colourblind and greyscale friendly)
 
 # set ID for The Simpsons - from https://www.imdb.com/title/tt0096697/
 simpsons_id <- "tt0096697"
+law_order_id <- "tt0098844"
+law_order_svu <- "tt0203259"
 
 # load ratings and episodes, change any \N to NA (have to do double slash to 'escape' the quotes)
 ratings <- read_tsv("data/title.ratings.tsv/data.tsv", na = "\\N")
 episodes <- read_tsv("data/title.episode.tsv/data.tsv", na = "\\N")
+#episode_names <- read_tsv("data/title.basics.tsv/data.tsv", na = "\\N")
+# note there will be ~61040 parsing errors
 
 # join the simpons episodes with their ratings
 # first create a new dataframe and put episodes into it
@@ -14,7 +19,58 @@ simpsons_episodes <- episodes %>%
   # filter this to only include episodes of the simpsons, by using its ID as the parent
   filter(parentTconst == simpsons_id) %>%
   # join the ratings dataframe onto the current one, where the episode IDs match (tconst)
+  # using inner join so that it only includes the episodes with ratings (otherwise would include 
+  # episodes that haven't aired yet)
   inner_join(ratings, by = "tconst")
 
-ggplot(data = simpsons_episodes, aes(x = numVotes, y = averageRating)) + geom_point(alpha = 0.2)
-  
+# Looks like there are 678 episodes that have aired - wow!
+# A check on Wikipedia says there are 679 to date (2020-04-08), but I found that Wikipedia treats
+# Season 28 Epsiode "The Great Phatsby" as two separate episodes (12 and 13), IMDb treats it as one (12)
+# so we're not missing an episode.
+
+# theme for the plot
+tile_theme = theme(
+  text = element_text(family = "Akbar"),
+  plot.background = element_rect(fill = "#FFD90F"),
+  panel.background = element_rect(fill=NA),
+  #panel.border = element_rect(fill=NA,color="black", size=0.5, linetype="solid"),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  axis.ticks = element_blank(), 
+  axis.text = element_text(color="black", size=8),
+  axis.text.y  = element_text(hjust=1),
+  legend.text = element_text(color="black", size=8),
+  legend.position = "top",
+  legend.background = element_rect(fill = NA),
+  #strip.background = element_rect(fill = "grey85", colour = NA),
+  legend.title=element_text())
+
+#ggplot(data = simpsons_episodes, aes(x = numVotes, y = averageRating)) + geom_point(alpha = 0.2)
+
+ggplot(data = simpsons_episodes, aes(x = seasonNumber, y = episodeNumber)) +
+  geom_tile(aes(fill = averageRating), colour = "black") +
+  geom_text(aes(label = format(averageRating, digits = 2)), size = 2) +
+  coord_fixed() +
+  scale_y_continuous(trans = "reverse",
+                     expand = c(0,0),
+                     breaks = 1:max(simpsons_episodes$episodeNumber),
+                     name = "Episode") +
+  scale_x_continuous(position = "top",
+                     expand = c(0,0),
+                     breaks = 1:max(simpsons_episodes$seasonNumber),
+                     name = "Season") +
+  scale_fill_viridis() + tile_theme
+
+
+
+num_eps <- simpsons_episodes %>%
+  group_by(seasonNumber) %>%
+  summarise(n = n()) %>%
+  ungroup()
+
+ggplot(data = ratings, aes(x = numVotes, y = averageRating)) + geom_point(alpha = 0.2)
+
+## install.packages(devtools)
+devtools::install_github("Ryo-N7/tvthemes")
+loadfonts(device='win')
+windowsFont()
